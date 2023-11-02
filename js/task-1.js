e137cdcd2651c564b35ddf18ee19cf51df6592e9
Chkana-Yaroslav <141314535+Chkana-Yaroslav@users.noreply.github.com>
2023-11-02 13:07:45 +0200

    hw-js-04

--- js/task-1.js
+++ js/task-1.js
@@ -1,16 +1,22 @@
-function slugify(title) {
-    const titleLowerCase = title.toLowerCase();
-    const titleSplit = titleLowerCase.split(" ");
-        
-    const titleJoin = titleSplit.join("-");
-    return titleJoin;
+function isEnoughCapacity(products, containerSize) {
+    let sumProducts = 0;
+    const values = Object.values(products);
+    
+    for (const value of values) {
+        sumProducts += value;
+    }
+
+    if (sumProducts <= containerSize) {
+        return true
+    }
+    return false;
 }
 
-     
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8))   
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); 
+
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14)); 
+
+console.log(isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7)); 
 
 
-
-console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
